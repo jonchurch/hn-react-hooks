1ca1bb782eb0fca3092766c26fbd6e2b06615fed
Jon Church <me@jonchurch.com>
2018-11-13 02:55:13 -0500

    add not found pure component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/Home.jsx'
 import Header from './components/Header.jsx'
 // import './App.css';
 
+const NotFound = () => <h1>404</h1>
+
 class App extends Component {
   render() {
     return (
@@ -17,10 +19,12 @@ class App extends Component {
 		<Route exact path="/show" component={Home} />
 		<Route exact path="/ask" component={Home} />
 		<Route exact path="/jobs" component={Home} />
+		<Route component={NotFound} />
 			</div>
 		</Router>
     );
   }
 }
+
 
 export default App;
