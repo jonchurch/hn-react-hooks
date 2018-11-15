645a41e2abe56391b165bef49d9c105b039ba831
Jon Church <me@jonchurch.com>
2018-11-14 22:35:10 -0500

    flesh out item component a little more

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import renderHTML from 'react-render-html'
 
+import { timeAgo } from '../helpers'
 import { useItemSubscription } from '../hooks'
 import Comment from './Comment'
 
 export default function Item({match}) {
 	const id = match.params.id
-	const { title, url, text, kids, score } = useItemSubscription(id)
+	const { title, by, url, time, text, kids = [], score } = useItemSubscription(id)
 	return (
 		<div>
 			{
 				url ? <a href={url}><h1>{title}</h1></a> :
 							<h1>{title}</h1>
 			}
-			<p>{score}</p>
+			<p>{score && score} points | by {by && by} {time && timeAgo(time)} ago</p>
 			{
 				text ?
 					renderHTML(text) : null
 			}
+			<hr />
 			{
-				kids && kids.length ? 
+				kids.length ? 
 					kids.map(Comment) : null
 			}
 	</div>
